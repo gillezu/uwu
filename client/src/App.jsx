fecd6cf2e6aed2c1caa59498b31ff90b66f91789
gillezu <gioelezu@gmail.com>
2024-11-14 08:38:50 +0100

    First GET request

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState("");
+
+  const fetchRouteApi = async () => {
+    try {
+      const response = await axios.get("/");
+      setData(response.data.message);
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+      setResponse("Error fetching data");
+    }
+    console.log(data);
+  };
 
   return (
     <>
@@ -18,9 +31,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={fetchRouteApi}>click Me</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -29,7 +40,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/app/server.py
+++ server/app/server.py
@@ -7,6 +7,12 @@ app = Flask(__name__)
 # Enable CORS for entire app
 CORS(app)
 
+
+@app.route("/")
+def hello_world():
+    return {"message": "Hello from backend"}, 200
+
+
 if __name__ == "__main__":
     # Run the Flask app, which starts the server
     app.run(host="0.0.0.0", port=5000, debug=True)
