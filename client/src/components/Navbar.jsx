57ae56b869ac13d9b2e9eebd9bc4531b9f7c5e06
gillezu <gioelezu@gmail.com>
2024-11-20 22:21:29 +0100

    Changed svgs to fontawesome

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,26 +1,35 @@
-import { statsImg, searchImg, barsImg } from "../utils"
-import { navLists } from "../constants"
+import { statsImg, searchImg, barsImg } from "../utils";
+import { navLists } from "../constants";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSquarePollVertical,
+  faMagnifyingGlass,
+  faBars,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
     <header className="w-full py-5 sm:px-10 px-5 flex justify-between items-center">
-        <nav className="flex w-full screen-max-width">
-            <img src={statsImg} alt="statistics" width={14} height={18} />
-            <div className="flex flex-1 justify-center max-sm:hidden">
-                {navLists.map((nav) => (
-                    <div key={nav} className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all">
-                        {nav}
-                    </div>
-                ))}
+      <nav className="flex w-full screen-max-width">
+        <FontAwesomeIcon icon={faSquarePollVertical} size="xl" />
+        <div className="flex flex-1 justify-center max-sm:hidden">
+          {navLists.map((nav) => (
+            <div
+              key={nav}
+              className="px-5 text-sm cursor-pointer text-gray hover:text-white transition-all"
+            >
+              {nav}
             </div>
+          ))}
+        </div>
 
-            <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
-                <img src={searchImg} alt="search" width={18} height={18} />
-                <img src={barsImg} alt="settings" width={18} height={18} />
-            </div>
-        </nav>
+        <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
+          <FontAwesomeIcon icon={faMagnifyingGlass} size="xl" />
+          <FontAwesomeIcon icon={faBars} size="xl" />
+        </div>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
