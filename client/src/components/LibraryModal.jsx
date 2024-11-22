70b52f811435eb6d95cccb8441de79ea38725638
gillezu <gioelezu@gmail.com>
2024-11-22 21:58:59 +0100

    Modal styling

--- client/src/components/LibraryModal.jsx
+++ client/src/components/LibraryModal.jsx
@@ -1,24 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBook, faClose } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBook,
+  faClose,
+  faMagnifyingGlass,
+} from "@fortawesome/free-solid-svg-icons";
 
 function LibraryModal({ onClose }) {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 backdrop-blur">
-      <div className="bg-black w-[50vw] h-[70vh] rounded shadow-lg flex flex-col  border-2 border-white">
-        <div className="flex justify-end">
-          <button
-            className="px-4 py-2 rounded text-white bg-transparent hover:bg-red-600 transition-colors duration-500 ease-in-out"
-            onClick={onClose}
-          >
-            <FontAwesomeIcon icon={faClose} size="2x" />
-          </button>
-        </div>
-        <div className="flex justify-center items-center">
-          <FontAwesomeIcon icon={faBook} size="3x" />
-
-          <h1 className="text-5xl mx-5">Library</h1>
-          <FontAwesomeIcon icon={faBook} size="3x" />
+      <div className="bg-black w-[50vw] h-[70vh] rounded shadow-lg flex flex-col border-2 border-white">
+        <div className="flex justify-between items-start p-5 border-b border-white">
+          <div className="w-1/3 flex items-center">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className="p-2 focus:outline-none hover:scale-105"
+            >
+              <FontAwesomeIcon icon={faMagnifyingGlass} size="xl" />
+            </button>
+            <input
+              type="text"
+              placeholder="Search"
+              className={`transition-all duration-300 ease-in-out text-white ${
+                isOpen ? "w-full ml-3 opacity-100" : "w-0 ml-0 opacity-0"
+              } px-3 py-2 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500`}
+            />
+          </div>
+          <div className="w-1/3 flex justify-center items-center">
+            <FontAwesomeIcon icon={faBook} size="3x" />
+            <h1 className="text-5xl mx-5 text-white">Library</h1>
+            <FontAwesomeIcon icon={faBook} size="3x" />
+          </div>
+          <div className="w-1/3 flex justify-end">
+            <button
+              className="px-4 py-2 rounded text-white bg-transparent border-2 hover:border-white
+              transition-colors duration-500 ease-in-out"
+              onClick={onClose}
+            >
+              <FontAwesomeIcon icon={faClose} size="2x" />
+            </button>
+          </div>
         </div>
       </div>
     </div>
