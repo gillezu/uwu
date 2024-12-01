4120642164717341f71ba2a1dd847963aa011ec2
gillezu <gioelezu@gmail.com>
2024-12-01 19:37:19 +0100

    Saving and deleting Patterns to DB finished

--- server/app/server.py
+++ server/app/server.py
@@ -3,15 +3,15 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_socketio import emit
 from grid import grid
+from routes.addPattern import add_pattern_bp
+from routes.applyPattern import apply_patterns_bp
+from routes.deletePattern import delete_pattern_bp
+from routes.getPatterns import send_patterns_bp
 from routes.initializeRandom import initialize_random_bp
-
+from routes.keyPress import key_press_bp
 from routes.mouseCoords import mouse_coords_bp
 from routes.nextGeneration import next_generation_bp
 from routes.resetGrid import reset_grid_bp
-from routes.keyPress import key_press_bp
-from routes.getPatterns import send_patterns_bp
-from routes.applyPattern import apply_patterns_bp
-from routes.addPattern import add_pattern_bp
 
 # Initialize app
 app = Flask(__name__)
@@ -33,6 +33,7 @@ app.register_blueprint(key_press_bp)
 app.register_blueprint(send_patterns_bp)
 app.register_blueprint(apply_patterns_bp)
 app.register_blueprint(add_pattern_bp)
+app.register_blueprint(delete_pattern_bp)
 
 
 if __name__ == "__main__":
